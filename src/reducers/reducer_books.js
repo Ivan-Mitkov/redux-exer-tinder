13d0417ce1a911a2b58acbc67d6d4b9908ec224c
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-09-30 10:15:17 +0300

    add reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default ()=>{
+    return([
+        {title:'Javascript the good parts'},
+        {title:'Eloquent Javascript'},
+        {title:'Secrets of the Javascript Ninja'},
+        {title:'React and Redux Explained'},
+    ])
+}
